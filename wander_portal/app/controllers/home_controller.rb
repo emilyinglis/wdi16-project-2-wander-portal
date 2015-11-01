616727412d626ca0c6e64ea87d4d55ba86afe6e3
emilyinglis <emilyameliainglis@gmail.com>
2015-11-01 17:34:35 +0000

    Testing git

---
+++ wander_portal/app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+
+  def about                   # define activity for the About page (unRESTful)
+  end
+
+  def contact                   # define activity for the About page (unRESTful)
+  end
+
+end
