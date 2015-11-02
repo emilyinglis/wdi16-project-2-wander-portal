472e2d3eaea6acc817a2f15c36d2340b1c280e6c
emilyinglis <emilyameliainglis@gmail.com>
2015-11-02 09:49:48 +0000

    Added role, name, description and logo columns into our strong params, so that when we submit the form they are accepted into our database

--- wander_portal/app/controllers/application_controller.rb
+++ wander_portal/app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
+# class ApplicationController < ActionController::Base
+#   # Prevent CSRF attacks by raising an exception.
+#   # For APIs, you may want to use :null_session instead.
+#   protect_from_forgery with: :exception
+# end
+
+
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    # my custom fields are :role, :name, :description, :logo
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:role, :name, :description, :logo, :email, :password, :password_confirmation)
+      end
+
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(:role, :name, :description, :logo, :email, :password, :password_confirmation, :current_password)
+      end
+    end
 end
