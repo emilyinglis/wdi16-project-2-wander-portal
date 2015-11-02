c0ed4daaa506b5e55f204ded74e4a9d97c2b293f
emilyinglis <emilyameliainglis@gmail.com>
2015-11-02 09:35:56 +0000

    With the aim of presenting the sizes available for a single product on the show pages, and potentially interacting with later on, I have created a join table with products and updated the 'has and belongs to many' relationships within the models.

--- wander_portal/app/models/product.rb
+++ wander_portal/app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+  has_and_belongs_to_many :sizes
+  has_and_belongs_to_many :categories
 end
---
+++ wander_portal/db/migrate/20151102092342_create_sizes_table.rb
@@ -0,0 +1,9 @@
+class CreateSizesTable < ActiveRecord::Migration
+  def change
+      create_table :sizes do |t|
+        t.string :name
+        
+        t.timestamps null: false
+      end
+    end
+end
