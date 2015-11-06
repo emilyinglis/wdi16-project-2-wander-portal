4841a8c322c9e86caa6ac6fb9cf05d57033a920b
emilyinglis <emilyameliainglis@gmail.com>
2015-11-06 17:00:32 +0000

    Filtered products shown on 'All Products' page by the signed in / current_user using 'current_user.products' in the product controller '

--- wander_portal/app/controllers/products_controller.rb
+++ wander_portal/app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = current_user.products
   end
 
   # GET /products/1
